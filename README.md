92527a7a8a09f3595197c63c2cb50f6c9e8c2f9d
cpuboi <80789492+cpuboi@users.noreply.github.com>
2022-11-13 11:08:16 +0100

    Added input arguments

--- README.md
+++ README.md
@@ -1,27 +1,43 @@
 # Memuniq #
 
-**uniq** but with memory, will only output lines that are unique to it. Handy when looping through same directories over and over.
+**uniq** but with memory, will only output lines that are unique to it.
 
 It uses a bloom filter which means it will never print a line it has seen before. 
 
 Default config is an error rate of 0.1% when 1 million items are added to the filter.  
 With this configuration memuniq uses about 5megs of RAM.
 
+## Usage ##
+```
+Usage of ./memuniq:
+  -f string
+    	Location of bloomfilter file (default "/home/user/.cache/bloomfilter.b64")
+  -i	Show statistics of processed lines
+  -n	Create a new filter and delete the old.
+  -p float
+    	Approximate error rate percentage, default 0.001% (default 0.001)
+  -s int
+    	Size of bloomfilter before major collissions occur, default: 1_000_000 (default 1000000)
+  -v	Show verbose information
+```
+
+
 ### Compiling ###
 ```
-go build -ldflags="-s -w" memuniq.go
+go build -o memuniq -ldflags="-s -w" main.go
 ```
+
+
+### Performance testing ###
+Generate a textfile:  
+```
+tr -dc "A-Za-z 0-9" < /dev/urandom | fold -w100|head -n 1000000 > ./1mil.txt
+cat ./1mil.txt | memuniq -i -v 
+```
+
 ### Shrinking the binary ###
 Install UPX to compress binary even further  
 This shrinks size from 1,6MB to 0,6MB   
 ```
 upx memuniq
 ```
-
-### Performance testing ###
-Generate a textfile:  
-```
-tr -dc "A-Za-z 0-9" < /dev/urandom | fold -w100|head -n 1000000 > ./1mil.txt
-cat ./1mil.txt | memuniq 
-```
-
--- memuniq.go
+++ memuniq.go
@@ -10,6 +10,20 @@ import (
 	bloom "github.com/EverythingMe/inbloom/go/inbloom"
 )
 
+// Load the good old input arguments
+func getArgs() (string, bool, bool, bool, int, float64) {
+
+	// This should handle alternative cache locations..
+	var b64FilePath = flag.String("f", os.ExpandEnv("$HOME/.cache/bloomfilter.b64"), "Location of bloomfilter file")
+	var showInfo = flag.Bool("i", false, "Show statistics of processed lines")
+	var beVerbose = flag.Bool("v", false, "Show verbose information")
+	var newBloomfilter = flag.Bool("n", false, "Create a new filter and delete the old.")
+	var sizeBloomfilter = flag.Int("s", 1_000_000, "Size of bloomfilter before major collissions occur, default: 1_000_000")
+	var percentageHitrate = flag.Float64("p", 0.001, "Approximate error rate percentage, default 0.001%")
+	flag.Parse()
+	return *b64FilePath, *showInfo, *beVerbose, *newBloomfilter, *sizeBloomfilter, *percentageHitrate
+}
+
 func main() {
 
 	logger := log.New(os.Stderr, "", 0)
@@ -17,35 +31,31 @@ func main() {
 	i := 0
 	hit := 0
 
-	// This section should handle several cache locations automatically
-	var b64FilePath = flag.String("f", os.ExpandEnv("$HOME/.cache/bloomfilter.b64"), "Location of bloomfilter file")
-	var showStatistics = flag.Bool("s", false, "Show statistics of processed lines")
-	var beVerbose = flag.Bool("v", false, "Show verbose information")
-	//var b64FilePath string = "/dev/shm/bloomfilter.b64"
-	flag.Parse()
-	var bf, err = bloom.NewFilter(10, 0.1)
+	b64FilePath, showInfo, beVerbose, newBloomfilter, sizeBloomfilter, percentageHitrate := getArgs()
+	var bf, err = bloom.NewFilter(sizeBloomfilter, percentageHitrate) // Create initial bloomfilter
 	if err != nil {
 		panic(err)
 	}
 
-	if _, err := os.Stat(string(*b64FilePath)); err == nil { // If file exists, load it.
-		file, err := os.ReadFile(*b64FilePath)
-		if err != nil {
-			panic(err)
+	// If user did not select new filter, try and find old filter and load it, otherwise continue with the newly generated filter.
+	if !newBloomfilter {
+		if _, err := os.Stat(string(b64FilePath)); err == nil { // If file exists, load it.
+			file, err := os.ReadFile(b64FilePath)
+			if err != nil {
+				panic(err)
+			}
+			bf, err = bloom.Unmarshal(file)
+			if err != nil {
+				panic(err)
+			}
+			if beVerbose {
+				logger.Println("Loading bloomfilter file", string(b64FilePath))
+			}
+		} else {
+			if beVerbose {
+				logger.Println("Did not find bloomfilter", string(b64FilePath), "creating new")
+			}
 		}
-		bf, err = bloom.Unmarshal(file)
-		if err != nil {
-			panic(err)
-		}
-		if *beVerbose {
-			fmt.Println("Loading bloomfilter file", string(*b64FilePath))
-		}
-	} else { // Create new bloomfilter
-		bf, err = bloom.NewFilter(1_000_000, 0.001)
-		if err != nil {
-			panic(err)
-		}
-
 	}
 
 	for scanner.Scan() { // For every line in input:
@@ -58,30 +68,27 @@ func main() {
 			i++
 			bf.Add(txtLine)
 		}
-
 	}
-	if *showStatistics {
+
+	if showInfo {
 		logger.Println("Bloomfilter duplicates:", hit, "Unique:", i) // Show statistics
 	}
 
 	//TODO: if can write to path, write:
 	// Can onle write to file if it already exists
-
-	//os.WriteFile(*b64FilePath, bf.Marshal(), 0644)
-
-	saveFile, err := os.Create(*b64FilePath)
+	saveFile, err := os.Create(b64FilePath)
 	if err != nil {
-		logger.Println("Could not create bloomfilter file", *b64FilePath)
+		logger.Println("Could not create bloomfilter file", b64FilePath)
 		panic(err)
 	}
 	defer saveFile.Close() // Make sure to close file.
 
 	bytesWritten, err := saveFile.Write(bf.Marshal())
 	if err != nil {
-		logger.Println("Could not save bloomfilter file", *b64FilePath)
+		logger.Println("Could not save bloomfilter file", b64FilePath)
 		panic(err)
 	}
-	if *beVerbose {
-		logger.Printf("Saved %d bytes in file %s\n", bytesWritten, *b64FilePath)
+	if beVerbose {
+		logger.Printf("Saved %d bytes in file %s\n", bytesWritten, b64FilePath)
 	}
 }
